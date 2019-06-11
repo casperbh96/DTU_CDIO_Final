782abfc8dbf9a246f103c54d85fc78a37eb886a2
casperbh96 <casperbh.96@gmail.com>
2019-06-11 10:36:43 +0200

    Changed call to column in database

--- src/main/java/DataAccess/dao/Connector.java
+++ src/main/java/DataAccess/dao/Connector.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class Connector {
     public static Connection static_createConnection() throws SQLException {
         return DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s185132"
-                + "?user=s185132&password=GL1TEuGDoXF6Lm1t2L7lQ");
+                + "?user=s185132&password=KkZA9ZkWxbJrJwZFnDIiy");
     }
     public static void static_startTransAction(Connection con) throws SQLException{
         con.setAutoCommit(false);
