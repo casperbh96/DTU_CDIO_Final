9ec4d1bba98471dda6605e0ba0c9a834b93c883e
casperbh96 <casperbh.96@gmail.com>
2019-06-12 20:55:46 +0200

    Added DTOs for relational tables (productB recipeB + recipe resource)

---
+++ src/main/java/DataAccess/dto/REL_RecipeResourceDTO.java
@@ -0,0 +1,48 @@
+package DataAccess.dto;
+
+public class REL_RecipeResourceDTO {
+    private int resourceBatchId;
+    private int productBatchId;
+    private double netAmount;
+    private double tara;
+
+    REL_RecipeResourceDTO(int resourceBatchId, int productBatchId, double netAmount, double tara){
+        this.resourceBatchId = resourceBatchId;
+        this.productBatchId = productBatchId;
+        this.netAmount = netAmount;
+        this.tara = tara;
+    }
+
+
+    public int getResourceBatchId() {
+        return resourceBatchId;
+    }
+
+    public void setResourceBatchId(int resourceBatchId) {
+        this.resourceBatchId = resourceBatchId;
+    }
+
+    public int getProductBatchId() {
+        return productBatchId;
+    }
+
+    public void setProductBatchId(int productBatchId) {
+        this.productBatchId = productBatchId;
+    }
+
+    public double getNetAmount() {
+        return netAmount;
+    }
+
+    public void setNetAmount(double netAmount) {
+        this.netAmount = netAmount;
+    }
+
+    public double getTara() {
+        return tara;
+    }
+
+    public void setTara(double tara) {
+        this.tara = tara;
+    }
+}
