b447f39ff75dce9a466a0d9c29f46c6fc225ee3c
casperbh96 <casperbh.96@gmail.com>
2019-06-13 12:01:07 +0200

    Updated user interface

--- src/main/java/BusinessLogic/I_User.java
+++ src/main/java/BusinessLogic/I_User.java
@@ -9,11 +9,15 @@ public interface I_User {
     UserDTO createUser(UserDTO singleUser) throws SQLException;
     List<UserDTO> createUsers( List<UserDTO> listOfUsers) throws SQLException;
 
-    UserDTO getSingleUserbyId(int userId ) throws SQLException;
-    List<UserDTO> getMultipleUsersByList (List<Integer> listOfUserIds) throws SQLException;
-    List<UserDTO> getUserbySearch(String keyword) throws SQLException;
+    UserDTO getUserById(int userId, boolean returnProductBatches, boolean returnAmounts) throws SQLException;
+    List<UserDTO> getUsersByListOfIds (List<Integer> listOfUserIds) throws SQLException;
+    List<UserDTO> getUserBySearch(String keyword) throws SQLException;
     List<UserDTO> getAllUsers() throws SQLException;
+    List<UserDTO> getAllUsers(boolean roles, boolean admins, boolean labTech, boolean pharmacist, boolean prodLeader) throws SQLException;
 
     UserDTO updateUser(UserDTO user) throws SQLException;
     List<UserDTO> updateUsers(List<UserDTO> listOfUsers) throws SQLException;
+
+    void deleteUser(int userId) throws SQLException;
+    void deleteUsers(List<Integer> userIds) throws SQLException;
 }
--- src/main/java/BusinessLogic/User.java
+++ src/main/java/BusinessLogic/User.java
@@ -15,6 +15,7 @@ public class User implements I_User{
         uDTO = new UserDTO(userId, username, initials, inactive);
     }
 
+
     @Override
     public UserDTO createUser(UserDTO singleUser) throws SQLException {
         return null;
@@ -26,27 +27,28 @@ public class User implements I_User{
     }
 
     @Override
-    public UserDTO getSingleUserbyId(int userId) throws SQLException {
+    public UserDTO getUserById(int userId, boolean returnProductBatches, boolean returnAmounts) throws SQLException {
         return null;
     }
 
     @Override
-    public List<UserDTO> getMultipleUsersByList(List<Integer> listOfUserIds) throws SQLException {
+    public List<UserDTO> getUsersByListOfIds(List<Integer> listOfUserIds) throws SQLException {
         return null;
     }
 
     @Override
-    public List<UserDTO> getUserbySearch(String keyword) throws SQLException {
+    public List<UserDTO> getUserBySearch(String keyword) throws SQLException {
         return null;
     }
 
+    @Override
     public List<UserDTO> getAllUsers() throws SQLException {
-        try{
-            return DAL_user.readAllUsers();
-        }
-        catch(SQLException ex){
-            throw new SQLException(ex);
-        }
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers(boolean roles, boolean admins, boolean labTech, boolean pharmacist, boolean prodLeader) throws SQLException {
+        return null;
     }
 
     @Override
@@ -57,5 +59,15 @@ public class User implements I_User{
     @Override
     public List<UserDTO> updateUsers(List<UserDTO> listOfUsers) throws SQLException {
         return null;
+    }
+
+    @Override
+    public void deleteUser(int userId) throws SQLException {
+
+    }
+
+    @Override
+    public void deleteUsers(List<Integer> userIds) throws SQLException {
+
     }
 }
