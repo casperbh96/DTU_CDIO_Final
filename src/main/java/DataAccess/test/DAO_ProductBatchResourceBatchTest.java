a5f3a416192a1135dbeea3f540d6185198c47db3
casperbh96 <casperbh.96@gmail.com>
2019-06-16 20:19:11 +0200

    Created the rest of the test classes (empty)

---
+++ src/main/java/DataAccess/test/DAO_ProductBatchResourceBatchTest.java
@@ -0,0 +1,20 @@
+package main.java.DataAccess.test;
+
+public class DAO_ProductBatchResourceBatchTest {
+    // region CREATE
+
+    // endregion
+
+    // region READ
+
+    // endregion
+
+    // region UPDATE
+
+    // endregion
+
+    // region DELETE
+
+    // endregion
+
+}
