06143774cebddc8ed6e4c0938464883d58e9367c
casperbh96 <casperbh.96@gmail.com>
2019-06-16 11:03:11 +0200

    Changed package reference

--- src/main/java/Rest/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package main.java.rest;
-
-import org.glassfish.jersey.server.ResourceConfig;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/rest")
-public class AppConfig extends ResourceConfig {
-}
