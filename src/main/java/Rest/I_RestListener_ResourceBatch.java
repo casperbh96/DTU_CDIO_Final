541a9bee5d698b840c7c0d4e59061d52ce7bb5c7
casperbh96 <casperbh.96@gmail.com>
2019-06-19 14:07:24 +0200

    Implemented REST ResourceBatch

--- src/main/java/Rest/I_RestListener_ResourceBatch.java
+++ src/main/java/Rest/I_RestListener_ResourceBatch.java
@@ -5,10 +5,10 @@ import main.java.Core.ResourceBatchDTO;
 import javax.ws.rs.core.Response;
 
 public interface I_RestListener_ResourceBatch {
-    Response createResourceBatch(ResourceBatchDTO recipe);
+    Response createResourceBatch(ResourceBatchDTO resourceBatch);
     Response getAllResourceBatches();
     Response getResourceBatchById(int resourceBatchId);
     Response getResourceBatchesBySearch(String search);
-    Response updateResourceBatch(ResourceBatchDTO user);
+    Response updateResourceBatch(ResourceBatchDTO resourceBatch);
     Response deleteResourceBatch(int resourceBatchId);
 }
