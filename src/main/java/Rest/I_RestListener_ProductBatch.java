e6320817e120bc0b332dbc8c5bde129c0ef5e12d
casperbh96 <casperbh.96@gmail.com>
2019-06-19 14:00:15 +0200

    Implemented REST ProductBatch

--- src/main/java/Rest/I_RestListener_ProductBatch.java
+++ src/main/java/Rest/I_RestListener_ProductBatch.java
@@ -5,10 +5,10 @@ import main.java.Core.ProductBatchDTO;
 import javax.ws.rs.core.Response;
 
 public interface I_RestListener_ProductBatch {
-    Response createProductBatch(ProductBatchDTO recipe);
+    Response createProductBatch(ProductBatchDTO productBatch);
     Response getAllProductBatches();
     Response getProductBatchById(int productBatchId);
     Response getProductBatchesBySearch(String search);
-    Response updateProductBatch(ProductBatchDTO user);
+    Response updateProductBatch(ProductBatchDTO productBatch);
     Response deleteProductBatch(int productBatchId);
 }
--- src/main/java/Rest/RestListener_ProductBatch.java
+++ src/main/java/Rest/RestListener_ProductBatch.java
@@ -7,6 +7,8 @@ import main.java.Core.ProductBatchDTO;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.List;
 
 @SuppressWarnings("unused")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,37 +19,85 @@ public class RestListener_ProductBatch implements I_RestListener_ProductBatch {
 
     @Path("create")
     @POST
-    public Response createProductBatch(ProductBatchDTO recipe) {
-        return null;
+    public Response createProductBatch(ProductBatchDTO productBatch) {
+        System.out.println(productBatch);
+
+        ProductBatchDTO returnProductBatch = null;
+        try{
+            returnProductBatch = productBatchBLL.createProductBatchButNotIncludedResourceBatches(productBatch);
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(returnProductBatch).build();
     }
 
     @Path("get")
     @GET
     public Response getAllProductBatches() {
-        return null;
+        List<ProductBatchDTO> productBatchList = null;
+        try{
+            productBatchList = productBatchBLL.getAllProductBatchs();
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(productBatchList).build();
     }
 
     @Path("get/{id}")
     @GET
     public Response getProductBatchById(@PathParam("id") int productBatchId) {
-        return null;
+        System.out.println(productBatchId);
+        ProductBatchDTO productBatch = null;
+        try{
+            productBatch = productBatchBLL.getProductBatchById(productBatchId);
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(productBatch).build();
     }
 
     @Path("get/search/{keyword}")
     @GET
     public Response getProductBatchesBySearch(@PathParam("keyword") String search) {
-        return null;
+        System.out.println(search);
+        List<ProductBatchDTO> productBatchList = null;
+        try{
+            productBatchList = productBatchBLL.getProductBatchBySearch(search);
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(productBatchList).build();
     }
 
     @Path("update")
     @PUT
-    public Response updateProductBatch(ProductBatchDTO user) {
-        return null;
+    public Response updateProductBatch(ProductBatchDTO productBatch) {
+        System.out.println(productBatch);
+        ProductBatchDTO returnProductBatch = null;
+        try{
+            returnProductBatch = productBatchBLL.updateProductBatchButNotResourceBatches(productBatch);
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(returnProductBatch).build();
     }
 
     @Path("delete/{id}")
     @DELETE
     public Response deleteProductBatch(@PathParam("id") int productBatchId) {
-        return null;
+        System.out.println(productBatchId);
+        ProductBatchDTO returnProductBatch = null;
+        try{
+            returnProductBatch = productBatchBLL.setInactiveSingleProductBatch(productBatchId);
+        } catch (SQLException ex) {
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(returnProductBatch).build();
     }
 }
