f4db3176dc5db280ef47cdecc5555404d73bc9b0
Grobaxgrappleblast <hansbyager@gmail.com>
2019-06-20 18:15:52 +0200

    virker med at sætte Options ind i Select

--- WebContent/js/TableCommands_ResourceTable.js
+++ WebContent/js/TableCommands_ResourceTable.js
@@ -51,6 +51,7 @@ var Res_STATE_editReady     ="edit";
 var Res_STATE_createReady   ="create";
 var Res_STATE_NOCHANGE      ="none";
 
+
 var RES_ID_NAMING = "resource_";
 var ROWINDEX = 1;
 
@@ -58,13 +59,13 @@ function HTML_GenerateResourceRow(Resource,RowName){
     return '\n' +
         '<tr class="DTO_Table_Row DTO_RESOURCE_GRID" id="'+RowName +'" data-editstate="'+Res_STATE_NOCHANGE+'" data-aktiveediting="false" >\n' +
         '            <td ><input  class="commit-state" type="checkbox"        name="checkBox" checked="unchecked" ></td>\n' +
-        '            <td class="dto-table-column-DTO-formElement"><div    class="resourceId"> '+Resource.resourceId+'</div>          </td>\n' +
-        '            <td class="dto-table-column-DTO-formElement"><input  class="resourceName" value="'+Resource.resourceName+'" disabled>   </td>\n' +
-        '            <td class="dto-table-column-DTO-formElement"><select class="reorder" disabled> \n' +
+        '            <td class="dto-table-column-DTO-formElement"><div      class="resourceId"> '+Resource.resourceId+'</div>          </td>\n' +
+        '            <td class="dto-table-column-DTO-formElement"><input    class="resourceName" value="'+Resource.resourceName+'" disabled>   </td>\n' +
+        '            <td class="dto-table-column-DTO-formElement"><select   class="reorder" disabled> \n' +
         '                <option value="true"> Re-Order </option>\n' +
         '                <option value="false"> No Re-Order </option>\n' +
         '            </select>\n' +
-        '            <td class="dto-table-column-DTO-formElement "><p value="'+Resource.inactive+'"> '+ Resource.inactive +' </p></td>\n' +
+        '            <td class="dto-table-column-DTO-formElement "><p       class="active" data-activestate=""  > INACTIVE DATA HERE </p></td>\n' +
         '            <td class="DTO_Table_Row_MenuBox" style="grid-row: 1/3; grid-column: 6/7;">\n' +
         '                <button class="dto-table-button resTableBtn_update" name="update" onclick="ResTable_UI_updateRow(this.parentElement.parentElement)" style="grid-row: 1/2;">update</button>\n' +
         '                <button class="dto-table-button resTableBtn_delete" name="delete" onclick="ResTable_UI_deleteRow(this.parentElement.parentElement)" style="grid-row: 2/3;">delete</button>\n' +
@@ -150,7 +151,16 @@ function LoadResources(){
         $.each(data, function (i, Resource) {
             var RowName = RES_ID_NAMING + ROWINDEX;
             $('#dto-table-container').append(HTML_GenerateResourceRow(Resource,RowName));
+
             $('#'+RowName+'').find('.commit-state').prop("checked",false);
+            var ActiveTag  = $('#'+RowName+'').find('.active');
+            if(Resource.inactive == false) {
+                ActiveTag.text("active");
+                ActiveTag.prop("data-activestate","false");
+            }else{
+                ActiveTag.text("in active ");
+                ActiveTag.prop("data-activestate","true");
+            }
             ROWINDEX = ROWINDEX +1;
         });
     })
@@ -160,20 +170,47 @@ function LoadResources(){
 function ResourceTable_REST_createResource(row){
     //ResTable_getNewId(function (data) {
 
-        var userdto ={
-            resourceId:  11 ,
+        var ResourceDTO ={
+            resourceId:  1123,
             resourceName:$(row).find('.resourceName').val(),
             reorder:$(row).find('.reorder').val(),
             inactive:false
         };
 
-        alert("resourceId :"+userdto.resourceId+" , resourceName:"+ userdto.resourceName+" , reorder:"+ userdto.reorder +"inactive :" + userdto.inactive)
-        /*post( JSON.stringify( userdto ) ,"rest/resources/create" , function (data) {
-            alert("created " + userdto.toString());
-        });*/
+        alert("resourceId :"+ ResourceDTO.resourceId +" , resourceName:"+ ResourceDTO.resourceName +" , reorder:"+ ResourceDTO.reorder +" ,inactive :" + ResourceDTO.inactive);
+        post( JSON.stringify( ResourceDTO ) ,"rest/resources/create" , function (data) {
+            alert("created " + ResourceDTO.toString());
+        });
 
     //});
 }
+function ResourceTable_REST_updateUser(row){
+
+
+    var ResourceDTO ={
+        resourceId:  $(row).find('.resourceId').text(),
+        resourceName:$(row).find('.resourceName').val(),
+        reorder:$(row).find('.reorder').val(),
+        inactive:false
+    };
+
+    alert("resourceId :"+ ResourceDTO.resourceId +" , resourceName:"+ ResourceDTO.resourceName +" , reorder:"+ ResourceDTO.reorder +" ,inactive :" + ResourceDTO.inactive);
+     put( JSON.stringify(ResourceDTO), "rest/resources/update" , function (data) {
+
+     });
+}
+function ResourceTable_REST_deleteUser(row){
+    var ResourceDTO ={
+        resourceId:  $(row).find('.resourceId').text(),
+        resourceName:$(row).find('.resourceName').val(),
+        reorder:$(row).find('.reorder').val(),
+        inactive:false
+    };
+    Delete( JSON.stringify(ResourceDTO), "rest/resources/delete" , function (data) {
+
+    });
+}
+
 function ResourceTable_commitTable(){
     //$(row).attr("data-editState")
     $('#dto-table-container').children('tr').each( function () {
@@ -185,12 +222,10 @@ function ResourceTable_commitTable(){
                 case Res_STATE_NOCHANGE:
                     break;
                 case Res_STATE_deleteReady:
-                    alert("delete not implemented");
-                   // userTable_REST_deleteUser( $(this) );
+                    ResourceTable_REST_deleteUser( $(this) );
                     break;
                 case Res_STATE_editReady:
-                    alert("update not implemented");
-                  //  userTable_REST_updateUser( $(this) );
+                    ResourceTable_REST_updateUser( $(this) );
                     break;
                 case Res_STATE_createReady:
                     ResourceTable_REST_createResource( $(this) );
@@ -201,36 +236,8 @@ function ResourceTable_commitTable(){
 }
 
 
-function userTable_REST_updateUser(row){
-    var userdto ={
-        userId:  row.find('.userId').text(),
-        username:row.find('.username').val(),
-        initials:row.find('.initials').val(),
-        inactive: row.attr("data-inactive-User")
-    };
-
-    put( JSON.stringify(userdto), "rest/users/update" , function (data) {
 
 
-        // First Delete all RoleRelations to this User
-        Delete(JSON.stringify(null), "rest/roleuser/delete/"+userdto.userId.toString().replace(/\s/g, '')   ,function () {
-            userTable_REST_addUserRoles_toUser(row, userdto)
-        });
-
-
-    });
-}
-function userTable_REST_deleteUser(row){
-    var userdto ={
-        userId:  row.find('.userId').text(),
-        username:row.find('.username').val(),
-        initials:row.find('.initials').val(),
-        inactive: row.attr("data-inactive-User")
-    };
-    Delete( JSON.stringify(userdto), "rest/users/delete" , function (data) {
-
-    });
-}
 
 function ResTable_getNewId( sfunc ){
     get("/rest/users/get/newid", function (data) {
