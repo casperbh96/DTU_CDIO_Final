ef1cb6a8352e8cc3562d3b6a2c2071bad0f06ea9
Grobaxgrappleblast <hansbyager@gmail.com>
2019-06-20 13:34:50 +0200

    Production Table og form er kodet så langt jeg kan, indtil Rest følger med

--- WebContent/js/ProductionFormJS.js
+++ WebContent/js/ProductionFormJS.js
@@ -44,15 +44,30 @@ function Delete(data, url, sFunc){
     });
 }
 
+var RECIPE_ID_NAME ="recipe_";
+var RECIPE_RES_ID_NAME ="res_Recipe_";
 var RECIPE_OPTIONS ="\n";
+var INGREDIENTS_OPTIONS ="\n";
+// Frequently Updated;
+var CURRENT_RECIPE;
+
 get('/rest/recipe/get', function (data) {
-    $.each(data, function (i, role) {
-        RECIPE_OPTIONS = RECIPE_OPTIONS + '<option value="volvo">Volvo</option> \n';
+    $.each(data, function (i, Recipe) {
+        RECIPE_OPTIONS = RECIPE_OPTIONS + '<option class="'+RECIPE_ID_NAME+Recipe.recipeId+'" value="'+ Recipe.recipeId +'" data-recipeId="'+Recipe.productAmount+'" data-authorId="'+Recipe.authorUserId+'">'+Recipe.recipeName+'</option> \n';
+        INGREDIENTS_OPTIONS = INGREDIENTS_OPTIONS + '<optgroup id="'+RECIPE_RES_ID_NAME+'" label="'+Recipe.recipeName+'">';
+        CURRENT_RECIPE = Recipe;
+
+        get('/rest/recipe/get/'+CURRENT_RECIPE.recipeId+'', function (data) {
+
+            INGREDIENTS_OPTIONS = INGREDIENTS_OPTIONS + '<option value="volvo">Volvo</option>';
+
+        });
+
     });
 });
 
 function HTML_GenerateForm(){
-    return
+    return '\n' +
     '    <form id="ProductionFormContainer">\n' +
         '        <h1> title </h1>\n' +
         '        <table class="ProductionForm_subPartContainer">\n' +
@@ -60,7 +75,7 @@ function HTML_GenerateForm(){
         '            <tr><td>\n' +
         '                <span>Recipe_id</span>\n' +
         '            </td><td>\n' +
-        '                <select>\n' +
+        '                <select id="ProductionForm_RecipeContainer">\n' +
         ''+  RECIPE_OPTIONS +
         '                </select>\n' +
         '            </td></tr>\n' +
@@ -93,11 +108,25 @@ function HTML_GenerateForm(){
 
 
 }
-$( document ).ready(function(){
-    $('#generatedForm').append(HTML_GenerateForm);
-});
+
+/* These are played in Sequence, each activated by previous */
+function start(){
+   $('#generatedForm').append(HTML_GenerateForm()).done(function() {
+       UI_setupOnChangeRecipe();
+   });
+}
+// on chance Recipe.
+function UI_setupOnChangeRecipe(){
+    $('#ProductionForm_RecipeContainer').change(function(){
 
 
 
-// get Recipes that you can choose from.
-// setting up a radio selection of options
+
+
+    });
+}
+
+// on change ingredients
+
+
+// select a ResouceBatch
