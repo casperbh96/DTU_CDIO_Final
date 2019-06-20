94eaeb10e15bf7f5147510ef0fa72863ccc12a88
casperbh96 <casperbh.96@gmail.com>
2019-06-20 15:17:24 +0200

    Implemented REST UserProductBatch

--- src/main/java/Rest/I_RestListener_UserProductBatch.java
+++ src/main/java/Rest/I_RestListener_UserProductBatch.java
@@ -5,8 +5,8 @@ import main.java.Core.REL_UserProductBatchDTO;
 import javax.ws.rs.core.Response;
 
 public interface I_RestListener_UserProductBatch {
-    Response createRoleUser(REL_UserProductBatchDTO roleUser);
+    Response createRoleUser(REL_UserProductBatchDTO userProductBatch);
     Response getAllRoleUsers();
     Response getRoleUserByUserId(int userId);
-    Response deleteRoleUser(REL_UserProductBatchDTO roleUser);
+    Response deleteRoleUser(REL_UserProductBatchDTO userProductBatch);
 }
--- src/main/java/Rest/RestListener_UserProductBatch.java
+++ src/main/java/Rest/RestListener_UserProductBatch.java
@@ -7,6 +7,9 @@ import main.java.Core.REL_UserProductBatchDTO;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @SuppressWarnings("unused")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -16,25 +19,58 @@ public class RestListener_UserProductBatch implements I_RestListener_UserProduct
     I_BLLUserProductBatch userProductBatchBLL = new BLLUserProductBatch();
     @Path("create")
     @POST
-    public Response createRoleUser(REL_UserProductBatchDTO roleUser) {
-        return null;
+    public Response createRoleUser(REL_UserProductBatchDTO userProductBatch) {
+        System.out.println(userProductBatch);
+
+        try{
+            userProductBatchBLL.assignSingleProductBatchUser(userProductBatch);
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(userProductBatch).build();
     }
 
     @Path("get")
     @GET
     public Response getAllRoleUsers() {
-        return null;
+        List<REL_UserProductBatchDTO> returnUserProductBatchList = null;
+        try{
+            returnUserProductBatchList = userProductBatchBLL.readAllProductBatchUsers();
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(returnUserProductBatchList).build();
     }
 
     @Path("get/{id}")
     @GET
     public Response getRoleUserByUserId(@PathParam("id") int userId) {
-        return null;
+        System.out.println(userId);
+
+        List<REL_UserProductBatchDTO> returnUserProductBatchList = null;
+        try{
+            returnUserProductBatchList = userProductBatchBLL.readProductBatchesByUserId(userId);
+
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok(returnUserProductBatchList).build();
     }
 
     @Path("delete")
     @DELETE
-    public Response deleteRoleUser(REL_UserProductBatchDTO roleUser) {
-        return null;
+    public Response deleteRoleUser(REL_UserProductBatchDTO userProductBatch) {
+        System.out.println("deleting users roles: " + userProductBatch.toString());
+
+        try{
+            userProductBatchBLL.deleteSingleUserProductBatch(userProductBatch.getUserId(), userProductBatch.getProductBatchId());
+        } catch (SQLException ex){
+            return Response.status(400).entity("SQLException: " + ex.getMessage()).build();
+        }
+
+        return Response.ok().build();
     }
 }
