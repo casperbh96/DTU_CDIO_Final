cbaef9364b33b4d3eb12f7b7764546e38f41e4df
Grobaxgrappleblast <hansbyager@gmail.com>
2019-06-21 08:13:54 +0200

    det virker !

--- WebContent/js/ProductionFormJS.js
+++ WebContent/js/ProductionFormJS.js
@@ -53,6 +53,7 @@ var CreateROW = 0;
 
 
 
+
 // HTML functions --- --- --- --- --- --- --- --- ---
 function HTML_setUpIngredients_Opt( container ){
     CreateROW = 0;
@@ -65,13 +66,13 @@ function HTML_setUpIngredients_Opt( container ){
 
     get('/rest/resources/get', function (data) {
 
-         $.each(data, function (u, Resource ) {
+        $.each(data, function (u, Resource ) {
 
-             //var tag = '<option value="test"> test </option>';
+            //var tag = '<option value="test"> test </option>';
             // var tag = '<option value="test"> test </option> \n ';
-             var tag = '<option value="'+ Resource.resourceId +'"  data-ResourceId="'+ Resource.resourceId +'" ' +
-                 'data-resourceName="'+ Resource.resourceName +'" data-reorder="'+ Resource.reorder +'" ' +
-                 'data-inactive="'+ Resource.inactive +'">'+ Resource.resourceName +'</option>';
+            var tag = '<option value="'+ Resource.resourceId +'"  data-ResourceId="'+ Resource.resourceId +'" ' +
+                'data-resourceName="'+ Resource.resourceName +'" data-reorder="'+ Resource.reorder +'" ' +
+                'data-inactive="'+ Resource.inactive +'">'+ Resource.resourceName +'</option>';
             sel.append($(tag));
         });
     });
@@ -79,29 +80,41 @@ function HTML_setUpIngredients_Opt( container ){
 function HTML_setUpRecipe_Options(container){
 
     var sel = $('<select>').prop("size","10");
-
     sel.prop("multiple","yes");
     sel.css("width","100%");
     sel.appendTo(container);
 
-
     get('/rest/recipe/get', function (data) {
 
-           $.each(data, function (i, Recipe) {
+        $.each(data, function (i, Recipe) {
 
-                //var tag = '<option value="test"> test </option>';
-                var tag = '<option onclick="switchResourcesView(this)" class="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
-                               'value="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
-                               'data-recipeId="'+ Recipe.recipeId +'" ' +
-                               'data-recipeEndDate="'+ Recipe.recipeEndDate +'" ' +
-                               'data-recipeName="'+ Recipe.recipeName +'" ' +
-                               'data-productAmount="'+ Recipe.productAmount +'" ' +
-                               'data-authorUserId="'+ Recipe.authorUserId +'">'+ Recipe.recipeName +'</option>\n';
-               CreateROW = CreateROW + 1;
-               sel.append($(tag));
+            //var tag = '<option value="test"> test </option>';
+            var tag = '<option onclick="switchResourcesView(this)" class="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
+                'value="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
+                'data-recipeId="'+ Recipe.recipeId +'" ' +
+                'data-recipeEndDate="'+ Recipe.recipeEndDate +'" ' +
+                'data-recipeName="'+ Recipe.recipeName +'" ' +
+                'data-productAmount="'+ Recipe.productAmount +'" ' +
+                'data-authorUserId="'+ Recipe.authorUserId +'">'+ Recipe.recipeName +'</option>\n';
+            CreateROW = CreateROW + 1;
 
-           });
-   });
+            sel.append($(tag));
+
+        });
+    });
+}
+function HTML_recipeResourceRow(container , ResourceDTO){
+
+
+    var html ='' +
+        '<tr>' +
+        '   <td data-R_ID="'+ResourceDTO.resourceId+'" data-ReORder="'+ResourceDTO.reorder+'" data-inactive="'+ResourceDTO.inactive+'" >'+ResourceDTO.resourceName+'</td>' +
+        '   <td><input class="Recipe_REL_resource" type="number" placeholder="ammount Kg"></td>' +
+        '   <td><input class="Recipe_REL_resource" type="number" placeholder="Tolerance%"></td>' +
+        '</tr>';
+    container.append(html);
+
+
 }
 // tables
 function HTML_CreateProduction_Form(){
@@ -167,8 +180,43 @@ function HTML_CreateResourceBach_Form(){
         '</form>';
 }
 function HTML_CreateRecipeBach_Form(){
-    return '';
+    return '' +
+        '<form class="Create_FormContainer" data-test="jeg er parent Containeren">\n' +
+        '    <h1> RECIPE </h1>\n' +
+        '    <table class="Creation_subPartContainer">\n' +
+        '        <tr>\n' +
+        '            <td>Recipe Name</td>\n' +
+        '            <td><input class="Recipe_name" type="text"></td>\n' +
+        '        </tr>\n' +
+        '        <tr>\n' +
+        '            <td>Ammount</td>\n' +
+        '            <td><input class="Recipe_Ammount" type="number "></td>\n' +
+        '        </tr>\n' +
+        '        <tr>\n' +
+        '            <td>Author id</td>\n' +
+        '            <td><input class="Recipe_Author" type="number"></td>\n' +
+        '        </tr>\n' +
+        '    </table>\n' +
+        '    <div class="Creation_subPartContainer " style="grid-column-gap:15px;">\n' +
+        '        <h1 style="margin-top: 15px;">Recipes Available</h1>\n' +
+        '        <div class="ProductionForm_ListContainer ProductionForm_ResourceContainer" style="">\n' +
+        '       </div>\n' +
+        '    <div class="Creation_subPartContainer " style="grid-column-gap:15px;">\n' +
+        '\n' +
+        '        <Button type="button" onclick="RecepyPop_AddResource(this.parentElement.parentElement.parentElement)" > add ingredient </Button>\n' +
+        '        <div class="ProductionForm_ListContainer ProductionForm_ResourceRelContainer" style="">\n' +
+        '           <table size="10" name="selectionField" multiple="yes" style="width: 100%">\n' +
+        '           </table>\n' +
+        '        </div>\n' +
+        '    </div>\n' +
+        '    <div class="Creation_subPartContainer" style="grid-column-gap:15px;">\n' +
+        '\n' +
+        '    </div>\n' +
+        '    <button type="submit" onclick="commitRecipeTable(this.parentElement.parentElement)" > commit </button>\n' +
+        '</form>';
 }
+//Commits --- --- --- --- --- --- --- --- --- ---
+
 
 //Commits --- --- --- --- --- --- --- --- --- ---
 function commitCreateTable(self){
@@ -188,37 +236,69 @@ function commitCreateTable(self){
             break;
     }
 }
-// Rest
 function commitResourceTable(table){
 
-   var ResourceDTO ={
-       resourceBatchId:  table.find('.ResourceDTO_BatchID').val(),
-       resourceBatchAmount:table.find('.ResourceDTO_Amount').val(),
-       supplierName:     table.find('.ResourceDTO_Supplier').val(),
-       isLeftover: "false",
-       resourceId:    table.find('#CreationForm_SelectContainer').find('select').val(),
+    var ResourceDTO ={
+        resourceBatchId:  table.find('.ResourceDTO_BatchID').val(),
+        resourceBatchAmount:table.find('.ResourceDTO_Amount').val(),
+        supplierName:     table.find('.ResourceDTO_Supplier').val(),
+        isLeftover: "false",
+        resourceId:    table.find('#CreationForm_SelectContainer').find('select').val(),
     };
 
 
-     get(JSON.stringify(ResourceDTO),'rest/resources/create',function (data){
-       alert("succes");
-     },function (data) {
-         alert(data);
-     });
+    get(JSON.stringify(ResourceDTO),'rest/resources/create',function (data){
+        alert("succes");
+    },function (data) {
+        alert(data);
+    });
+}
+function commitRecipeTable(container){
+
+    //todo CREATE RECIPE DTO AND RELS.
+    /*  var ResourceDTO ={
+          resourceBatchId:  table.find('.ResourceDTO_BatchID').val(),
+          resourceBatchAmount:table.find('.ResourceDTO_Amount').val(),
+          supplierName:     table.find('.ResourceDTO_Supplier').val(),
+          isLeftover: "false",
+          resourceId:    table.find('#CreationForm_SelectContainer').find('select').val(),
+      };*/
+
+    /*  get(JSON.stringify(ResourceDTO),'rest/resources/create',function (data){
+          alert("succes");
+      },function (data) {
+          alert(data);
+      });*/
 }
 
+
+
 // Useability Functions
+function RecepyPop_AddResource(container){
+
+    var resource = $(container).find('.ProductionForm_ResourceContainer select option:selected');
+
+    var ResourceDTO = {
+        resourceId: resource.attr("data-resourceid"),
+        resourceName: resource.attr("data-resourceName"),
+        reorder: resource.attr("data-reorder"),
+        inactive: resource.attr("data-inactive")
+    };
+    if(typeof resource.attr("data-resourceid") !== "undefined") {
+        HTML_recipeResourceRow($(container).find('.ProductionForm_ResourceRelContainer table'), ResourceDTO);
+    }
+}
 //production
 function switchResourcesView(self){
 // get/resources/resourcebatches/{recipeid}
 
-    alert("recipe ID : "+ $(self).attr("data-recipeId") );
+    // alert("recipe ID : "+ $(self).attr("data-recipeId") );
 
     get('/rest/recipe/get/resources/resourcebatches/'+$(self).attr("data-recipeId")+'', function (data) {
 
         $.each(data, function (u, ResourceAndBath) {
 
-            alert("got some");
+            //alert("got some");
             /*//var tag = '<option value="test"> test </option>';
             var tag = '<option onclick="switchResourcesView(this)" class="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
                 'value="'+RECIPE_OPTION_CLASSNAME+CreateROW+'" ' +
@@ -233,7 +313,6 @@ function switchResourcesView(self){
         });
     });
 }
-
 
 // Useability Functions
 function RecepyPop_AddResource(container){
@@ -268,12 +347,14 @@ function pop_resourceForm(self){
     }
 }
 function pop_recipeForm(self){
+    alert("hej");
     if($(self).attr("data-active") === "true"){
         $('#overlay').empty();
         $(self).attr("data-active","false");
     }else{
         var html = HTML_CreateRecipeBach_Form();
         $('#overlay').append(html);
+        alert("append");
         var SelectContainer = $('.ProductionForm_ResourceContainer');
         HTML_setUpIngredients_Opt(SelectContainer);
         $(self).attr("data-active","true");
