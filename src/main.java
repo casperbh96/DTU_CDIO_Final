015afc0753b9c18fe806ddf79fadab4b1fac1d78
Grobaxgrappleblast <hansbyager@gmail.com>
2019-06-22 11:04:53 +0200

    tilføjet Reference til MAIN

--- src/main.java
+++ src/main.java
@@ -7,6 +7,8 @@ public class main{
 
     private static Tomcat tomcat;
 
+    //https://github.com/cbudtz/02327_WebProject2/blob/master/src/main/java/Main.java
+
     public static void main(String[] args) throws LifecycleException {
         tomcat = new Tomcat();
         tomcat.setBaseDir("temp");
